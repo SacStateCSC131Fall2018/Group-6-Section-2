1ce3f29dc770ee25a4789382728ad499a5120d23
jacob <jacob@DESKTOP-K7RPJSL>
2018-12-01 21:32:15 -0800

    added menubar help and file. File has a menubaritem exit that exits the program.

--- CSC131PROJECT/src/PirexGUI.java
+++ CSC131PROJECT/src/PirexGUI.java
@@ -11,8 +11,42 @@ public class PirexGUI extends JFrame {
        Container     contentPane;
        JPanel        search, load, summarize;
        JTabbedPane   tabbedPane;
+       	
+       //Menu bar
+	    JMenuBar menuBar;
+		JMenu file, help;
+		JMenuItem exit, about;
+		menuBar = new JMenuBar();
+	
+		file = new JMenu("File");	
+		exit = new JMenuItem(new AbstractAction("exit")
+		{
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}	
+		});
+		file.add(exit);
+		menuBar.add(file);
+		
+		help = new JMenu("Help");
+		
+		about = new JMenuItem(new AbstractAction("about")
+				{
+					public void actionPerformed(ActionEvent e) {
+						JFrame helpWindow = new JFrame();
+						helpWindow.setTitle("Help");
+						helpWindow.setSize(300,400);
+						helpWindow.setVisible(true);
+						JLabel text = new JLabel("This is a test run for the help menu", JLabel.CENTER);
+						helpWindow.add(text);
+					}
+				});
+		help.add(about);
+		menuBar.add(help);
+		
+		setJMenuBar(menuBar);
       
-       //Pirex header
+		//Pirex header
        ImageIcon icon = new ImageIcon("icons/icon.png");       
        setIconImage(icon.getImage());
        setTitle("Pirex");
