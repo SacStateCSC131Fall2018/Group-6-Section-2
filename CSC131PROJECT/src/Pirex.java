529fad3625d1e5e4902b6cf02bd165384d326425
trist <trist@10.0.0.149>
2018-12-01 18:18:36 -0800

    Changed names of files to Pirex and PirexGUI.

---
+++ CSC131PROJECT/src/Pirex.java
@@ -0,0 +1,21 @@
+
+
+/**
+ * An example that uses a JTabbedPane
+ *
+ */
+public class Pirex {
+
+    /**
+     * The entry point of the example
+     *
+     * @param args   The command line arguments
+     */
+    public static void main(String[] args) {
+        PirexGUI   gui;
+
+        gui = new PirexGUI();
+        gui.setSize(400,200);
+        gui.setVisible(true);
+    }
+}
